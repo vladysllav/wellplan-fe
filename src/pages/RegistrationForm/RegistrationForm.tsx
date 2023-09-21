28b9ba179f10a0a2b4839d8d0aa255b054a5fc7e
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-09-21 19:54:35 +0300

    change css in @apply

--- src/pages/RegistrationForm/RegistrationForm.tsx
+++ src/pages/RegistrationForm/RegistrationForm.tsx
@@ -6,9 +6,10 @@ import 'react-datepicker/dist/react-datepicker.css';
 import Button from '../../components/Button/Button';
 import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './RegistrationForm.css'
 
 
-interface FormValues {
+ interface FormValues {
     firstName: string;
     lastName: string;
     email: string;
@@ -79,11 +80,9 @@ const NewPage: React.FC = () => {
             <form onSubmit={formik.handleSubmit} className="mt-6 space-y-4 flex justify-center flex-col m-auto w-max">
                 <div className="relative h-10 w-full min-w-[200px] mt-5">
                     <input
-                        className=" peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent
-                         bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0
-                          transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200
-                           placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-indigo-500
-                            focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                        className="input-field peer border-blue-gray-200 text-blue-gray-700
+                        placeholder-shown:border-blue-gray-200
+                        placeholder-shown:border-t-blue-gray-200 disabled:bg-blue-gray-50"
                         placeholder=" "
                         id="firstName"
                         name="firstName"
@@ -92,21 +91,9 @@ const NewPage: React.FC = () => {
                         onBlur={formik.handleBlur}
                         value={formik.values.firstName}
                     />
-                    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5
-                     flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400
-                      transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border
-                       before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l
-                        before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px]
-                         after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md
-                          after:border-t after:border-r after:border-blue-gray-200 after:transition-all
-                           peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75]
-                            peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent
-                             peer-placeholder-shown:after:border-transparent peer-focus:text-[11px]
-                              peer-focus:leading-tight peer-focus:text-indigo-500 peer-focus:before:border-t-2
-                               peer-focus:before:border-l-2 peer-focus:before:border-indigo-500 peer-focus:after:border-t-2
-                                peer-focus:after:border-r-2 peer-focus:after:border-indigo-500 peer-disabled:text-transparent
-                                 peer-disabled:before:border-transparent peer-disabled:after:border-transparent
-                                  peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                    <label className="label_field before:content[' '] after:content[' ']
+                    text-blue-gray-400 before:border-blue-gray-200 after:border-blue-gray-200
+                    peer-placeholder-shown:text-blue-gray-500 peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                         First Name
                     </label>
                 </div>
@@ -116,11 +103,9 @@ const NewPage: React.FC = () => {
                 <div className="relative h-10 w-full min-w-[200px] mt-5">
                     <input
                         placeholder=" "
-                        className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent
-                         bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0
-                          transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200
-                           placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-indigo-500
-                            focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                        className="input-field peer border-blue-gray-200 text-blue-gray-700
+                        placeholder-shown:border-blue-gray-200
+                        placeholder-shown:border-t-blue-gray-200 disabled:bg-blue-gray-50"
                         id="lastName"
                         name="lastName"
                         type="text"
@@ -128,21 +113,9 @@ const NewPage: React.FC = () => {
                         onBlur={formik.handleBlur}
                         value={formik.values.lastName}
                     />
-                    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5
-                     flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400
-                      transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border
-                       before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l
-                        before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px]
-                         after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md
-                          after:border-t after:border-r after:border-blue-gray-200 after:transition-all
-                           peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75]
-                            peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent
-                             peer-placeholder-shown:after:border-transparent peer-focus:text-[11px]
-                              peer-focus:leading-tight peer-focus:text-indigo-500 peer-focus:before:border-t-2
-                               peer-focus:before:border-l-2 peer-focus:before:border-indigo-500 peer-focus:after:border-t-2
-                                peer-focus:after:border-r-2 peer-focus:after:border-indigo-500 peer-disabled:text-transparent
-                                 peer-disabled:before:border-transparent peer-disabled:after:border-transparent
-                                  peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                    <label className="label_field before:content[' '] after:content[' ']
+                    text-blue-gray-400 before:border-blue-gray-200 after:border-blue-gray-200
+                    peer-placeholder-shown:text-blue-gray-500 peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                         Last name
                     </label>
                 </div>
@@ -151,11 +124,9 @@ const NewPage: React.FC = () => {
                 ) : null}
                 <div className="relative h-10 w-full min-w-[200px] mt-5">
                     <input
-                        className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent
-                         bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0
-                          transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200
-                           placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-indigo-500
-                            focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                        className="input-field peer border-blue-gray-200 text-blue-gray-700
+                        placeholder-shown:border-blue-gray-200
+                        placeholder-shown:border-t-blue-gray-200 disabled:bg-blue-gray-50"
                         id="email"
                         name="email"
                         type="email"
@@ -164,21 +135,9 @@ const NewPage: React.FC = () => {
                         onBlur={formik.handleBlur}
                         value={formik.values.email}
                     />
-                    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5
-                     flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400
-                      transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border
-                       before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l
-                        before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px]
-                         after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md
-                          after:border-t after:border-r after:border-blue-gray-200 after:transition-all
-                           peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75]
-                            peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent
-                             peer-placeholder-shown:after:border-transparent peer-focus:text-[11px]
-                              peer-focus:leading-tight peer-focus:text-indigo-500 peer-focus:before:border-t-2
-                               peer-focus:before:border-l-2 peer-focus:before:border-indigo-500 peer-focus:after:border-t-2
-                                peer-focus:after:border-r-2 peer-focus:after:border-indigo-500 peer-disabled:text-transparent
-                                 peer-disabled:before:border-transparent peer-disabled:after:border-transparent
-                                  peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                    <label className="label_field before:content[' '] after:content[' ']
+                    text-blue-gray-400 before:border-blue-gray-200 after:border-blue-gray-200
+                    peer-placeholder-shown:text-blue-gray-500 peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                         Email
                     </label>
                 </div>
@@ -217,11 +176,9 @@ const NewPage: React.FC = () => {
                 </div>
                 <div className="relative h-10 w-full min-w-[200px] mt-5">
                     <input
-                        className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent
-                         bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0
-                          transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200
-                           placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-indigo-500
-                            focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                        className="input-field peer border-blue-gray-200 text-blue-gray-700
+                        placeholder-shown:border-blue-gray-200
+                        placeholder-shown:border-t-blue-gray-200 disabled:bg-blue-gray-50"
                         placeholder=" "
                         id="password"
                         name="password"
@@ -230,21 +187,9 @@ const NewPage: React.FC = () => {
                         onBlur={formik.handleBlur}
                         value={formik.values.password}
                     />
-                    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5
-                     flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400
-                      transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border
-                       before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l
-                        before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px]
-                         after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md
-                          after:border-t after:border-r after:border-blue-gray-200 after:transition-all
-                           peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75]
-                            peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent
-                             peer-placeholder-shown:after:border-transparent peer-focus:text-[11px]
-                              peer-focus:leading-tight peer-focus:text-indigo-500 peer-focus:before:border-t-2
-                               peer-focus:before:border-l-2 peer-focus:before:border-indigo-500 peer-focus:after:border-t-2
-                                peer-focus:after:border-r-2 peer-focus:after:border-indigo-500 peer-disabled:text-transparent
-                                 peer-disabled:before:border-transparent peer-disabled:after:border-transparent
-                                  peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                    <label className="label_field before:content[' '] after:content[' ']
+                    text-blue-gray-400 before:border-blue-gray-200 after:border-blue-gray-200
+                    peer-placeholder-shown:text-blue-gray-500 peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                         Password
                     </label>
                 </div>
