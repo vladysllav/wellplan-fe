a15feaf41fff4aa6a602f71aec5af65942f17e67
Vladyslav Kozanchyn <v.kozanchyn@gmail.com>
2023-09-21 21:30:14 +0200

    WEL-12: update, move search logic and structure to component

--- src/components/DoctorCardList/DoctorCardList.tsx
+++ src/components/DoctorCardList/DoctorCardList.tsx
@@ -1,31 +1,38 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useMemo, useState} from 'react';
+import Search from '../Search/Search';
+import useDebounce from '../../hooks/useDebounce';
 import DoctorCard from '../DoctorCard/DoctorCard';
 import {dummyDoctorsList} from '../../data/dummyDoctorsList';
 import {DoctorModel} from '../../models/doctor.model';
 
 const DoctorCardList = () => {
     const [doctors, setDoctors] = useState<DoctorModel[]>([]);
-    const [loading, setLoading] = useState(true);
     const [searchQuery, setSearchQuery] = useState('');
-    const [filteredDoctors, setFilteredDoctors] = useState<DoctorModel[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [isFiltering, setIfFiltering] = useState(true);
+
+    const debouncedSearchQuery = useDebounce<string>(searchQuery, 1000);
 
     useEffect(() => {
         setDoctors(dummyDoctorsList);
-        setFilteredDoctors(dummyDoctorsList);
         setLoading(false);
     }, []);
 
-    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const query = event.target.value.toLowerCase();
-        setSearchQuery(query);
-
-        const filtered = doctors.filter((doctor) => {
+    const filteredDoctors = useMemo(() => {
+        setIfFiltering(true)
+        return doctors.filter((doctor) => {
             const fullName = doctor.fullName.toLowerCase();
             const specialization = doctor.specialization.toLowerCase();
-            return fullName.includes(query) || specialization.includes(query);
+            return fullName.includes(debouncedSearchQuery) || specialization.includes(debouncedSearchQuery);
         });
+    }, [doctors, debouncedSearchQuery]);
 
-        setFilteredDoctors(filtered);
+    useEffect(() => {
+        setIfFiltering(false);
+    }, [filteredDoctors]);
+
+    const handleSearchChange = (query: string) => {
+        setSearchQuery(query);
     };
 
     return (
@@ -35,26 +42,7 @@ const DoctorCardList = () => {
             ) : (
                 <>
                     <div className="relative mb-4">
-                        <input
-                            type="text"
-                            placeholder="Search by name or specialization"
-                            className="w-full px-4 py-2 pl-10 pr-8 border rounded-md shadow-sm focus:outline-none focus:ring-1 focus:border-violet-700"
-                            value={searchQuery}
-                            onChange={handleSearchChange}/>
-                        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <i className="fas fa-search text-gray-400"></i>
-                        </div>
-                        {searchQuery !== '' && (
-                            <div className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer">
-                                <i
-                                    className="fas fa-times text-gray-400 hover:text-red-500"
-                                    onClick={() => {
-                                        setSearchQuery('');
-                                        setFilteredDoctors(doctors);
-                                    }}
-                                ></i>
-                            </div>
-                        )}
+                        <Search onSearch={handleSearchChange}></Search>
                     </div>
                     <div>
                         {filteredDoctors.length === 0 ? (
@@ -66,9 +54,9 @@ const DoctorCardList = () => {
                             </div>
                         ) : (
                             <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                                {filteredDoctors.map((doctor: DoctorModel, idx: number) => (
-                                    <div key={idx}
-                                         className="w-full">
+                                {filteredDoctors.map((doctor: DoctorModel) => (
+                                    <div key={doctor.id}
+                                         className={`w-full transition-opacity duration-700 ease-in-out ${isFiltering ? 'opacity-0' : 'opacity-100'}`}>
                                         <DoctorCard doctor={doctor}/>
                                     </div>
                                 ))}
