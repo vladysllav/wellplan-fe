82c9b4173a1c7437027b5dac98f6742af5b6af96
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-09-24 22:17:19 +0300

    fix forms

--- src/pages/AuthorizationForm/AuthorizationForm.tsx
+++ src/pages/AuthorizationForm/AuthorizationForm.tsx
@@ -1,9 +1,11 @@
-
+import '../../assets/styles/Tailwind.css'
 import {useFormik} from "formik";
 import * as yup from "yup";
 import {toast, ToastContainer} from "react-toastify";
 import Button from "../../components/Button/Button";
 import {Link, useNavigate} from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 
 const Login: React.FC = () => {
@@ -15,38 +17,39 @@ const Login: React.FC = () => {
             email: '',
             password: '',
         },
-
         validationSchema: yup.object({
             email: yup.string().email('Invalid email address').required('Required'),
-            password: yup.string().required('Required'),
+            password: yup
+                .string()
+                .required('Required')
+                .min(8, 'Password must be at least 8 characters long')
+                .max(15, 'Password cannot exceed 15 characters')
+                .matches(/^[a-zA-Z0-9]+$/, 'Password cannot contain special characters')
         }),
-
-        onSubmit: async (values) => {
-            try {
-                if (values.email && values.password) {
-                    console.log("Form data", values);
-                    navigate("/");
-                } else {
-                    toast.error("Authentication failed: Please fill in all fields");
-                }
-            } catch (error) {
-                console.error("An error occurred:", error);
-            }
-        },
+        onSubmit: values => {
+            console.log('Form data', values);
+            navigate("/");
+        }
     });
 
-
+    const handleButtonClick = () => {
+        if (formik.errors) {
+            toast.error("Please fix the form errors before submitting.");
+        } else {
+            console.log("Form data", formik.values);
+            navigate("/");
+        }
+    };
     return (
         <div className="min-h-screen">
+           <Header/>
             <div className="my-6 border-t border-gray-200"></div>
             <h2 className="text-2xl mb-4 font-semibold text-blue-700">Login</h2>
             <ToastContainer/>
             <form onSubmit={formik.handleSubmit} className="mt-6 space-y-4 flex justify-center flex-col m-auto w-max">
                 <div className="relative h-10 w-full min-w-[200px] mt-5">
                     <input
-                        className="input-field peer border-blue-gray-200 text-blue-gray-700
-                placeholder-shown:border-blue-gray-200
-                placeholder-shown:border-t-blue-gray-200 disabled:bg-blue-gray-50"
+                        className="input-field peer my_input"
                         id="email"
                         name="email"
                         type="email"
@@ -55,9 +58,7 @@ const Login: React.FC = () => {
                         onBlur={formik.handleBlur}
                         value={formik.values.email}
                     />
-                    <label className="label_field before:content[' '] after:content[' ']
-                text-blue-gray-400 before:border-blue-gray-200 after:border-blue-gray-200
-                peer-placeholder-shown:text-blue-gray-500 peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                    <label className="label_field my_label">
                         Email
                     </label>
                 </div>
@@ -68,9 +69,7 @@ const Login: React.FC = () => {
                 }
                 <div className="relative h-10 w-full min-w-[200px] mt-5">
                     <input
-                        className="input-field peer border-blue-gray-200 text-blue-gray-700
-                            placeholder-shown:border-blue-gray-200
-                            placeholder-shown:border-t-blue-gray-200 disabled:bg-blue-gray-50"
+                        className="input-field peer my_input"
                         placeholder=" "
                         id="password"
                         name="password"
@@ -79,9 +78,7 @@ const Login: React.FC = () => {
                         onBlur={formik.handleBlur}
                         value={formik.values.password}
                     />
-                    <label className="label_field before:content[' '] after:content[' ']
-                            text-blue-gray-400 before:border-blue-gray-200 after:border-blue-gray-200
-                            peer-placeholder-shown:text-blue-gray-500 peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                    <label className="label_field my_label">
                         Password
                     </label>
                 </div>
@@ -91,13 +88,14 @@ const Login: React.FC = () => {
                     ) : null
                 }
                 <div className="mt-4 pb-5">
-                    <Button label="Submit" type="submit"/>
+                    <Button label="Submit" type="submit" onClick={handleButtonClick}/>
                     <div>
                         <Link className="text-lg font-semibold text-green-500 hover:text-orange-500"
                               to="/forgot-password">Forgot your password?</Link>
                     </div>
                 </div>
             </form>
+            <Footer/>
         </div>
     )
 }
